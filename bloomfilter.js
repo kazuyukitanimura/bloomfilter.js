c68f4766d7d8d4769ad936a4879bd4cffc29caab
kazuyukitanimura <kazuyukitanimura@gmail.com>
2011-12-01 02:24:43 +0000

    integrated murmurhash

--- bloomfilter.js
+++ bloomfilter.js
@@ -1,3 +1,5 @@
+var murmurhash3 = require('./murmurhash-js/murmurhash3_gc');
+
 function BloomFilter(m, k){
   this.m = m;
   this.k = k;
@@ -7,37 +9,13 @@ function BloomFilter(m, k){
   while (++i < n) buckets[i] = 0;
 }
 
-// Fowler/Noll/Vo hashing.
-function fnv_1a(v){
-  var n = v.length;
-  var a = 2166136261;
-  var c;
-  var i = -1;
-  while (++i < n){
-    c = v.charCodeAt(i);
-    a ^= (c & 0xff00) >> 8;
-    a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
-    a &= 0xffffffff;
-    a ^= c & 0xff;
-    a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
-    a &= 0xffffffff;
-  }
-  return a;
-}
-
-// One additional iteration of FNV, given a hash.
-function fnv_1a_b(a){
-  a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
-  return a & 0xffffffff;
-}
-
 // See http://willwhim.wordpress.com/2011/09/03/producing-n-hash-functions-by-hashing-only-once/
 BloomFilter.prototype.locations = function(v){
   var k = this.k;
   var m = this.m;
   var r = [];
-  var a = fnv_1a(v);
-  var b = fnv_1a_b(a);
+  var a = murmurhash3(v, 0);
+  var b = murmurhash3(v, 1);
   var i = -1;
   while (++i < k) r[i] = (a + b * i) % m;
   return r;
--- test/bloomfilter-test.js
+++ test/bloomfilter-test.js
@@ -1,19 +1,30 @@
 var BloomFilter = require("../bloomfilter");
+var RandomURLString = require('../murmurhash-js/lib/RandomURLString');
 
 var vows = require("vows");
 var assert = require("assert");
 
 var suite = vows.describe("bloomfilter");
 
+var nTests = 256;
+var keys = [];
+
+/**
+ * Initializing the keys
+ */
+for(var i=nTests; i--;){
+  keys.push(RandomURLString(4));
+}
+
 suite.addBatch({
   "bloom filter": {
     "basic": function() {
-      var f = new BloomFilter(1000, 4);
-      var n1 = "Bess";
-      var n2 = "Jane";
-      f.add(n1);
-      assert.equal(f.test(n1),  true);
-      assert.equal(f.test(n2), false);
+      var f = new BloomFilter(10000, 4);
+      for(var i=nTests/2; i--;){
+        f.add(keys[i]);
+        assert.equal(f.test(keys[i           ]),  true);
+        assert.equal(f.test(keys[i + nTests/2]), false);
+      }
     },
     "basic uint32": function() {
       var f = new BloomFilter(1000, 4);
