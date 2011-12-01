cf93e9719eec1b3befe9696484eb0bbe26b1d6d9
kazuyukitanimura <kazuyukitanimura@gmail.com>
2011-12-01 05:47:46 +0000

    clean up

--- bloomfilter.js
+++ bloomfilter.js
@@ -4,40 +4,28 @@ function BloomFilter(m, k){
   this.m = m;
   this.k = k;
   var buckets = this.buckets = [];
-  var n = Math.ceil(m / k);
-  for(var i = n; i--;){
+  for(var i = Math.ceil(m / 32); i--;){ // note that the precision of javascript is 53bits
     buckets[i] = 0;
   }
 }
 
-BloomFilter.prototype.locations = function(v){
-  var m = this.m;
-  var r = [];
-  for(var i = this.k; i--;){
-    r[i] = murmurhash3(v, i) % m;
-  }
-  return r;
-};
-
 BloomFilter.prototype.add = function(v){
-  var l = this.locations(v);
-  var k = this.k;
+  var m = this.m;
   var b;
   var buckets = this.buckets;
   for(var i = this.k; i--;){
-    b = l[i];
-    buckets[Math.floor(b / k)] |= 1 << (b % k)
+    b = murmurhash3(v, i) % m;
+    buckets[Math.floor(b / 32)] |= 1 << (b % 32);
   }
 };
 
 BloomFilter.prototype.test = function(v){
-  var l = this.locations(v);
-  var k = this.k;
+  var m = this.m;
   var b;
   var buckets = this.buckets;
   for(var i = this.k; i--;){
-    b = l[i];
-    if((buckets[Math.floor(b / k)] & (1 << (b % k))) === 0){
+    b = murmurhash3(v, i) % m;
+    if((buckets[Math.floor(b / 32)] & (1 << (b % 32))) === 0){
       return false;
     }
   }
