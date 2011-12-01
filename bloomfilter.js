79ccc3e6b7ab7ec6208a87693a5b29b08cc114c6
kazuyukitanimura <kazuyukitanimura@gmail.com>
2011-12-01 01:40:14 +0000

    spacing

--- bloomfilter.js
+++ bloomfilter.js
@@ -1,74 +1,72 @@
-(function(exports) {
-  function BloomFilter(m, k) {
-    this.m = m;
-    this.k = k;
-    var buckets = this.buckets = [],
-        n = Math.ceil(m / k),
-        i = -1;
-    while (++i < n) buckets[i] = 0;
-  }
+function BloomFilter(m, k){
+  this.m = m;
+  this.k = k;
+  var buckets = this.buckets = [];
+  var n = Math.ceil(m / k);
+  var i = -1;
+  while (++i < n) buckets[i] = 0;
+}
 
-  // Fowler/Noll/Vo hashing.
-  function fnv_1a(v) {
-    var n = v.length,
-        a = 2166136261,
-        c,
-        i = -1;
-    while (++i < n) {
-      c = v.charCodeAt(i);
-      a ^= (c & 0xff00) >> 8;
-      a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
-      a &= 0xffffffff;
-      a ^= c & 0xff;
-      a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
-      a &= 0xffffffff;
-    }
-    return a;
-  }
-
-  // One additional iteration of FNV, given a hash.
-  function fnv_1a_b(a) {
+// Fowler/Noll/Vo hashing.
+function fnv_1a(v){
+  var n = v.length;
+  var a = 2166136261;
+  var c;
+  var i = -1;
+  while (++i < n){
+    c = v.charCodeAt(i);
+    a ^= (c & 0xff00) >> 8;
     a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
-    return a & 0xffffffff;
+    a &= 0xffffffff;
+    a ^= c & 0xff;
+    a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
+    a &= 0xffffffff;
   }
+  return a;
+}
 
-  // See http://willwhim.wordpress.com/2011/09/03/producing-n-hash-functions-by-hashing-only-once/
-  BloomFilter.prototype.locations = function(v) {
-    var k = this.k,
-        m = this.m,
-        r = [],
-        a = fnv_1a(v),
-        b = fnv_1a_b(a),
-        i = -1;
-    while (++i < k) r[i] = (a + b * i) % m;
-    return r;
-  };
+// One additional iteration of FNV, given a hash.
+function fnv_1a_b(a){
+  a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
+  return a & 0xffffffff;
+}
 
-  BloomFilter.prototype.add = function(v) {
-    var l = this.locations(v),
-        i = -1,
-        k = this.k,
-        buckets = this.buckets;
-    while (++i < k) buckets[Math.floor(l[i] / k)] |= 1 << (l[i] % k)
-  };
+// See http://willwhim.wordpress.com/2011/09/03/producing-n-hash-functions-by-hashing-only-once/
+BloomFilter.prototype.locations = function(v){
+  var k = this.k;
+  var m = this.m;
+  var r = [];
+  var a = fnv_1a(v);
+  var b = fnv_1a_b(a);
+  var i = -1;
+  while (++i < k) r[i] = (a + b * i) % m;
+  return r;
+};
 
-  BloomFilter.prototype.test = function(v) {
-    var l = this.locations(v),
-        n = l.length,
-        i = -1,
-        k = this.k,
-        b,
-        buckets = this.buckets;
-    while (++i < n) {
-      b = l[i];
-      if ((buckets[Math.floor(b / k)] & (1 << (b % k))) === 0) {
-        return false;
-      }
+BloomFilter.prototype.add = function(v){
+  var l = this.locations(v);
+  var i = -1;
+  var k = this.k;
+  var buckets = this.buckets;
+  while (++i < k) buckets[Math.floor(l[i] / k)] |= 1 << (l[i] % k)
+};
+
+BloomFilter.prototype.test = function(v){
+  var l = this.locations(v);
+  var n = l.length;
+  var i = -1;
+  var k = this.k;
+  var b;
+  var buckets = this.buckets;
+  while (++i < n){
+    b = l[i];
+    if((buckets[Math.floor(b / k)] & (1 << (b % k))) === 0){
+      return false;
     }
-    return true;
-  };
+  }
+  return true;
+};
 
-  exports.BloomFilter = BloomFilter;
-  exports.fnv_1a = fnv_1a;
-  exports.fnv_1a_b = fnv_1a_b;
-})(typeof exports !== "undefined" ? exports : this);
+if(typeof module !== "undefined"){
+  module.exports = BloomFilter;
+}
--- test/bloomfilter-test.js
+++ test/bloomfilter-test.js
@@ -1,27 +1,27 @@
-var BloomFilter = require("../bloomfilter").BloomFilter;
+var BloomFilter = require("../bloomfilter");
 
-var vows = require("vows"),
-    assert = require("assert");
+var vows = require("vows");
+var assert = require("assert");
 
 var suite = vows.describe("bloomfilter");
 
 suite.addBatch({
   "bloom filter": {
     "basic": function() {
-      var f = new BloomFilter(1000, 4),
-          n1 = "Bess",
-          n2 = "Jane";
+      var f = new BloomFilter(1000, 4);
+      var n1 = "Bess";
+      var n2 = "Jane";
       f.add(n1);
-      assert.equal(f.test(n1), true);
+      assert.equal(f.test(n1),  true);
       assert.equal(f.test(n2), false);
     },
     "basic uint32": function() {
-      var f = new BloomFilter(1000, 4),
-          n1 = "\u100",
-          n2 = "\u101",
-          n3 = "\u103";
+      var f = new BloomFilter(1000, 4);
+      var n1 = "\u100";
+      var n2 = "\u101";
+      var n3 = "\u103";
       f.add(n1);
-      assert.equal(f.test(n1), true);
+      assert.equal(f.test(n1),  true);
       assert.equal(f.test(n2), false);
       assert.equal(f.test(n3), false);
     }
