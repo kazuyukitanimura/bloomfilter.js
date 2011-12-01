d9f23abe6a7ddd0b1b3ad0b98fcd72bd7d2ce8dd
kazuyukitanimura <kazuyukitanimura@gmail.com>
2011-12-01 05:32:50 +0000

    %m

--- bloomfilter.js
+++ bloomfilter.js
@@ -5,35 +5,37 @@ function BloomFilter(m, k){
   this.k = k;
   var buckets = this.buckets = [];
   var n = Math.ceil(m / k);
-  var i = -1;
-  while (++i < n) buckets[i] = 0;
+  for(var i = n; i--;){
+    buckets[i] = 0;
+  }
 }
 
 BloomFilter.prototype.locations = function(v){
-  var k = this.k;
   var m = this.m;
   var r = [];
-  var i = -1;
-  while (++i < k) r[i] = murmurhash3(v, i);
+  for(var i = this.k; i--;){
+    r[i] = murmurhash3(v, i) % m;
+  }
   return r;
 };
 
 BloomFilter.prototype.add = function(v){
   var l = this.locations(v);
-  var i = -1;
   var k = this.k;
+  var b;
   var buckets = this.buckets;
-  while (++i < k) buckets[Math.floor(l[i] / k)] |= 1 << (l[i] % k)
+  for(var i = this.k; i--;){
+    b = l[i];
+    buckets[Math.floor(b / k)] |= 1 << (b % k)
+  }
 };
 
 BloomFilter.prototype.test = function(v){
   var l = this.locations(v);
-  var n = l.length;
-  var i = -1;
   var k = this.k;
   var b;
   var buckets = this.buckets;
-  while (++i < n){
+  for(var i = this.k; i--;){
     b = l[i];
     if((buckets[Math.floor(b / k)] & (1 << (b % k))) === 0){
       return false;
